947f38c7f4b1b566c87f422cf527051d6ae08d91
gaurang <gaurangagarwal557@gmail.com>
2021-01-03 21:37:32 +0530

    first commit

--- app.py
+++ app.py
@@ -11,10 +11,14 @@ def function():
 
 @app.route("/submit",methods=["POST"])
 def submit():
-	result=None
-	inp=request.form['hours']
-	result=imp.predict(float(inp))
-	return render_template("index.html",result=result)
+    result=None
+    inp=float(request.form['hours'])
+    result=imp.predict(inp/60.0)[0][0]
+    if float(result)>100.0:
+        result="Woah you will get full 100 marks if you study that much!!!"
+    else:
+        result="your score is {}".format(result)    
+    return render_template("index.html",result=result)
 
 if __name__=='__main__':
 	app.run(debug=True)
